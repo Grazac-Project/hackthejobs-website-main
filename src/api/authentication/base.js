c32bf72ce59ee0ec3fdc529ce52d154629984ec8
abiodunolayinka <abiodunolayinka75@gmail.com>
2025-12-01 20:29:58 +0100

    fix: Correct environment URL selection logic and update webinar registration modal UI with new dependency and disabled overflow handling.

--- src/api/authentication/base.js
+++ src/api/authentication/base.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const isProduction = process.env.NEXT_PUBLIC_DOMAIN_DEV
 
 export const authKit = axios.create({
-  baseURL: isProduction === "development"
+  baseURL: isProduction !== "development"
     ? process.env.NEXT_PUBLIC_API_URL_DEV 
     : process.env.NEXT_PUBLIC_API_URL_PROD, 
   timeout: 50000,
@@ -13,7 +13,7 @@ export const authKit = axios.create({
 });
 
 export const authKit2 = axios.create({
-  baseURL: isProduction === "development"
+  baseURL: isProduction !== "development"
     ? process.env.NEXT_PUBLIC_API_URL_DEV
     : process.env.NEXT_PUBLIC_API_URL_PROD,
   timeout: 50000,
